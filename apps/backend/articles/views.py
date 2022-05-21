95daf79ad344b0eb3b068afc74ef091dcc51cfeb
nia <leit8@mail.ru>
2022-05-21 12:51:37 +0300

    user story 3 completed

--- apps/backend/articles/views.py
+++ apps/backend/articles/views.py
@@ -1,4 +1,5 @@
 from rest_framework.views import APIView, Response
+from rest_framework.pagination import PageNumberPagination
 
 from articles.models import Article
 from articles.serializers import ArticleListPreviewSerializer
@@ -7,5 +8,8 @@ from articles.serializers import ArticleListPreviewSerializer
 class ArticleListAPIView(APIView):
     def get(self, request, *args, **kwargs):
         queryset = Article.objects.all()
+        pg = PageNumberPagination()
+        pg.page_size_query_param = "page_size"
+        queryset = pg.paginate_queryset(queryset, request)
         serializer = ArticleListPreviewSerializer(queryset, many=True)
         return Response(serializer.data, status=200)
--- apps/backend/tests/test_article/test_views.py
+++ apps/backend/tests/test_article/test_views.py
@@ -1,6 +1,7 @@
 import json
 
 import pytest
+import random
 from django.contrib.auth import get_user_model
 
 from articles.models import Article
@@ -11,16 +12,7 @@ User = get_user_model()
 @pytest.mark.django_db
 class TestArticle:
     def test_get_news(self, api_client):
-        Article.objects.create(
-            title='title',
-            title_trans='title_trans',
-            article=1,
-            preview='preview',
-            author=User.objects.create(username='test_user_for_article'),
-            body='body',
-            short_summary='short_summary',
-            views=0
-        )
+        self.create_article()
 
         response = api_client().get("/api/v1/articles/articles")
         articles = response.data
@@ -33,3 +25,27 @@ class TestArticle:
             assert "views" in article
             assert "rating" in article
 
+    def create_article(self):
+
+        Article.objects.create(
+            title='title',
+            title_trans='title_trans',
+            article=1,
+            preview='preview',
+            author=User.objects.create(username=f'user {random.randint(0, 9999)}'),
+            body='body',
+            short_summary='short_summary',
+            views=0
+        )
+
+    def test_pagination_news(self, api_client):
+        page_size = 2
+        for _ in range(page_size * 3):
+            self.create_article()
+        response = api_client().get(f"/api/v1/articles/articles?page_size={page_size}")
+        response2 = api_client().get(f"/api/v1/articles/articles?page_size={page_size}&page=2")
+        articles = response.data
+        assert response.status_code == 200
+        assert len(articles) == page_size
+        assert response2.data != response.data
+        assert list([x["id"] for x in response.data]) != list([x["id"] for x in response2.data])
