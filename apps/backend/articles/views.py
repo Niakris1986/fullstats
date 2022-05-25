d44b6ed5453dd3198f2c988ea8248958e5e1db59
nia <leit8@mail.ru>
2022-05-25 21:55:00 +0300

    user stories added

--- apps/backend/articles/views.py
+++ apps/backend/articles/views.py
@@ -1,15 +1,49 @@
-from rest_framework.views import APIView, Response
-from rest_framework.pagination import PageNumberPagination
+from django.db.models import Count, F, Q
+from rest_framework import permissions
+from rest_framework.filters import OrderingFilter
+from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveUpdateDestroyAPIView, CreateAPIView
 
-from articles.models import Article
-from articles.serializers import ArticleListPreviewSerializer
+from articles.models import Article, LikeChoices, LikeArticle
+from articles.serializers import ArticleListPreviewSerializer, ArticleDetailSerializer, \
+    LikeArticleCreateUpdateSerializer
+from utils.pagination_classes import ArticlePagination
 
 
-class ArticleListAPIView(APIView):
-    def get(self, request, *args, **kwargs):
-        queryset = Article.objects.all()
-        pg = PageNumberPagination()
-        pg.page_size_query_param = "page_size"
-        queryset = pg.paginate_queryset(queryset, request)
-        serializer = ArticleListPreviewSerializer(queryset, many=True)
-        return Response(serializer.data, status=200)
+class ArticleListAPIView(ListAPIView):
+    serializer_class = ArticleListPreviewSerializer
+    pagination_class = ArticlePagination
+    filter_backends = [OrderingFilter]
+    ordering_fields = ['rating', 'publish']
+
+    def get_queryset(self):
+        queryset = Article.objects.all().annotate(
+            plus=Count('likes', filter=Q(likes__rating=LikeChoices.POSITIVE.value)),
+            neg=Count('likes', filter=Q(likes__rating=LikeChoices.NEGATIVE.value))
+        ).annotate(rating=F('plus') - F('neg'))
+
+        return queryset
+
+
+class ArticleRetrieveAPIView(RetrieveAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleDetailSerializer
+
+    def get_queryset(self):
+        queryset = Article.objects.all().annotate(
+            plus=Count('likes', filter=Q(likes__rating=LikeChoices.POSITIVE.value)),
+            neg=Count('likes', filter=Q(likes__rating=LikeChoices.NEGATIVE.value))
+        ).annotate(rating=F('plus') - F('neg'))
+        return queryset
+
+
+class LikeCreateDeleteArticleAPIView(CreateAPIView, RetrieveUpdateDestroyAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = LikeArticle.objects.all()
+    serializer_class = LikeArticleCreateUpdateSerializer
+    partial = True
+
+    def perform_create(self, serializer):
+        serializer.save(article_id=self.kwargs['pk'], user=self.request.user)
+
+    def get_object(self):
+        return LikeArticle.objects.get(pk=self.kwargs['pk'], user=self.request.user)
