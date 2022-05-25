04dc7283cd1238150248ce5168539bfe750c01ad
nia <leit8@mail.ru>
2022-05-25 21:54:47 +0300

    user stories added

--- apps/backend/tests/test_user/test_views.py
+++ apps/backend/tests/test_user/test_views.py
@@ -1,5 +1,6 @@
 import pytest
 from django.contrib.auth import get_user_model
+from faker import Faker
 
 User = get_user_model()
 
@@ -7,16 +8,19 @@ User = get_user_model()
 @pytest.mark.django_db
 class TestUser:
     def test_register_new_user(self, api_client):
+        faker = Faker()
+        profile = faker.profile()
         test_json = {
-            'username': 'test_user',
-            'password': 'superstrong_password123',
-            'email': 'test@mail.ru'
+            'username': profile['username'],
+            'password': profile['ssn'],
+            'email': profile['mail']
         }
 
+        print(test_json)
         response = api_client().post("/api/v1/users/users", data=test_json, format='json')
-        user = User.objects.filter(username='test_user').first()
+        user = User.objects.filter(username=profile['username']).first()
 
         assert response.status_code == 201
         assert user is not None
-        assert user.email == 'test@mail.ru'
-        assert user.check_password('superstrong_password123')
+        assert user.email == profile['mail']
+        assert user.check_password(profile['ssn'])
