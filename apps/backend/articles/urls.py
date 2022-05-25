3fd5088f96a31bd55c694e5e85b81b330978eac4
nia <leit8@mail.ru>
2022-05-25 21:54:50 +0300

    user stories added

--- apps/backend/articles/urls.py
+++ apps/backend/articles/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from articles.views import ArticleListAPIView
+from articles.views import ArticleListAPIView, ArticleRetrieveAPIView, LikeCreateDeleteArticleAPIView
 
 urlpatterns = [
-    path('articles', ArticleListAPIView.as_view(), name='articles-list')
+    path('articles', ArticleListAPIView.as_view(), name='articles-list'),
+    path('articles/<int:pk>', ArticleRetrieveAPIView.as_view(), name='articles-detail'),
+    path('articles/<int:pk>/like', LikeCreateDeleteArticleAPIView.as_view(), name='articles-like'),
 ]
+
