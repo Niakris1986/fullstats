5149bf6d1d658b531edda93ad27a205692d310bd
nia <leit8@mail.ru>
2022-05-25 21:51:48 +0300

    user stories added

--- apps/backend/articles/serializers.py
+++ apps/backend/articles/serializers.py
@@ -1,20 +1,25 @@
-from typing import Iterable
-
 from rest_framework import serializers
 
 from articles.models import Article, LikeArticle
 
 
 class ArticleListPreviewSerializer(serializers.ModelSerializer):
-    rating = serializers.SerializerMethodField()
-
-    def get_rating(self, obj: Article):
-        ratings: Iterable[LikeArticle] = obj.likes.all()
-        result = 0
-        for rating in ratings:
-            result += rating.rating
-        return result
+    rating = serializers.IntegerField()
 
     class Meta:
         model = Article
         fields = ['id', 'title', 'preview', 'short_summary', 'views', 'rating']
+
+
+class ArticleDetailSerializer(serializers.ModelSerializer):
+    rating = serializers.IntegerField()
+
+    class Meta:
+        model = Article
+        fields = ['id', 'title', 'preview', 'body', 'views', 'rating']
+
+
+class LikeArticleCreateUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LikeArticle
+        fields = ['rating']
